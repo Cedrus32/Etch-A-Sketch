98529ff355eebb64e39625a2de162e2912915df1
Shannon <srosenb@protonmail.ch>
2022-01-25 17:58:21 -0600

    Log grid-items

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 // GENERATE GRID //
 // ------------- //
 
+let gridItems;
+//...draw grid elements & create array of grid items
 function drawGrid(gridWH) {
     let gridSize = (gridWH ** 2)
     for (let i=1; i<=gridSize; i++) {
@@ -9,14 +11,16 @@ function drawGrid(gridWH) {
         const gridItem = document.createElement('div');
         gridItem.classList.add('grid-item');
         gridItem.textContent = i;
-        console.log(gridItem);
         //append grid item to grid container
         gridContainer.appendChild(gridItem);
-        console.log(gridContainer);
+        // gridItems.push(gridItem);
     }
-    // gridContainer.style.grid = '50px';
+    console.log(gridContainer);
+    const gridItems = gridContainer.querySelectorAll('div.grid-item');
+    console.log(gridItems);
 }
 
+//...layout grid structure
 const layoutGrid = (gridWH) => {
     gridContainer.style.gridTemplateColumns = `repeat(${gridWH}, 50px)`;
     gridContainer.style.gridTemplateRows = `repeat(${gridWH}, 50px)`;
@@ -30,6 +34,19 @@ function genGrid(userInput) {
     //draw grid...
     drawGrid(userInput);
 }
+
+// ------------ //
+// HOVER EFFECT //
+// ------------ //
+
+// function changeBG(e) {
+//     //TODO change bg
+//     e.classList.add('change-bg');
+// }
+
+// gridItems.forEach(div => div.addEventListener('mouseover', (e) => {
+//     console.log(e);
+// }));
 
 // ----------- //
 // INIT SCRIPT //
