2e12fd615a98a0f12614e7961bc0abc6aa48ffdf
Shannon <srosenb@protonmail.ch>
2022-01-30 09:58:52 -0600

    Consolidate script files

--- script.js
+++ script.js
@@ -1,106 +1,131 @@
-// ------------- //
-// DRAW EFFECTS //
-// ------------- //
+// ------------------------- //
+// LISTEN FOR DRAW & EFFECTS //
+// ------------------------- //
 
-// TODO include rainbow draw
-// TODO include color picker
-
-function changeBG() {
-    switch (colorMode) {
-        case 'rainbow':
-            gridItems.forEach(div => div.addEventListener('mouseover', () => {
-                div.classList.add('red-bg');
-                // div.style.backgroundColor = 'red';
-            }));
-            break;
-        case '':
-            gridItems.forEach(div => div.addEventListener('mouseover', () => {
-                div.classList.add('white-bg');
-                // div.style.backgroundColor = 'white';
-            }));
-    }
-    // gridItems.forEach(div => div.addEventListener('mouseover', () => {
-    //     div.classList.add('change-bg');
-    // }));
+//...erase color
+function drawErase() {
+    gridItems.forEach(div => div.addEventListener('mouseenter', () => {
+        div.style.backgroundColor = '';
+    }));
 }
 
-function stopChangeBG() {
-    gridContainer.removeEventListener('mouseover', changeBG);
-    // gridItems.forEach(div => div.addEventListener('mouseenter', () => {
-    //     div.style.backgroundColor = '';
-    // }));
-}
-
-function startChangeBG() {
-    gridContainer.addEventListener('mouseover', changeBG);
-    // gridItems.forEach(div => div.addEventListener('mouseenter', () => {
-    //     div.style.backgroundColor = 'pink';
-    // }));
-}
-
+//...clear grid & reset draw...
 function clearGrid() {
-    gridItems.forEach(div => div.setAttribute('class', 'grid-item'));
-    // gridItems.forEach(div => () => {
-    //     div.style.backgroundColor = '';
-    // });
-    gridContainer.setAttribute('class', 'grid-container');
-    console.log(false);
+    //set all gridItem BGs to ''
+    gridItems.forEach(div => div.style.backgroundColor = '');
+    //reset gridContainer class list
+    gridContainer.setAttribute('class', 'grid-container')
+    drawOn = false;
+    //stop listening for draw mode switch-case on mouseover --> erase
+    gridContainer.removeEventListener('mouseover', startDraw);
+    drawErase();
+
+    console.log(mode);
+    console.log(color);
+    console.log(drawOn);
 }
 
-let colorMode;
-//listen for effects...
-function listenForEffects() {
-    //listen for container click...
+//...draw in rainbow color
+function drawRainbow() {
+    gridItems.forEach(div => div.addEventListener('mouseenter', () => {
+        div.style.backgroundColor = 'red';
+    }));
+}
+
+//...draw in picker color
+function drawPicker() {
+    gridItems.forEach(div => div.addEventListener('mouseenter', () => {
+        div.style.backgroundColor = 'white';
+    }));
+}
+
+//...switch-case on mouseover...
+function startDraw() {
+    switch (mode) {
+        case 'picker':
+            drawPicker();
+            break;
+        case 'rainbow':
+            drawRainbow();
+    }
+}
+
+//...toggle draw
+function toggleDraw() {
+    gridContainer.classList.toggle('draw');
+    drawOn = gridContainer.classList.contains('draw');
+    return drawOn
+}
+
+//...check draw on...
+let mode;
+let color;
+let drawOn;
+let clear;
+function checkDrawOn() {
     gridContainer.addEventListener('click', () => {
-        gridContainer.classList.toggle('draw-on');
-        // let classListLength = gridContainer.classList.length;
-        let drawOn = gridContainer.classList.contains('draw-on');
-        switch (drawOn) {
-            case true:
-                console.log(drawOn);
-                startChangeBG(colorMode);
-                break;
-            case false:
-                //TODO removeEventListener does not work
-                console.log(drawOn);
-                stopChangeBG(colorMode);
+        //toggle draw class...
+        toggleDraw();
+    
+        console.log(mode);
+        console.log(color);
+        console.log(drawOn);
+
+        //check if draw is truthy/falsey...
+        if (drawOn === true) {
+            //listen for draw mode switch-case on mouseover --> draw
+            gridContainer.addEventListener('mouseover', startDraw);
+        } else if (drawOn === false) {
+            //stop listening for draw mode switch-case on mouseover --> erase
+            gridContainer.removeEventListener('mouseover', startDraw);
+            drawErase();
         }
     });
+}
 
-    const btnPicker = document.querySelector('#picker');
-    //listen for picker...
+//...listen for draw effects...
+const btnPicker = document.querySelector('#picker');
+const btnRainbow = document.querySelector('#rainbow');
+const btnClear = document.querySelector('#clear');
+function listenForEffects() {
+    //listen for picker & draw in picker color...
     btnPicker.addEventListener('click', () => {
-        colorMode = '';
-        console.log(colorMode);
+        mode = 'picker';
+        color = '"white"';
     });
-
-    //TODO colorMode does not work
-    const btnRainbow = document.querySelector('#rainbow');
-    //listen for rainbow...
+    //listen for rainbow & draw in rainbow color...
     btnRainbow.addEventListener('click', () => {
-        colorMode += 'rainbow';
-        console.log(colorMode);
+        mode = 'rainbow';
+        color = '"red"';
     });
+    //listen for clear & clear grid/reset draw...
+    btnClear.addEventListener('click', () => {
+        // mode = 'clear';
+        // color = '';
+        clearGrid();
+    });
+}
 
-    const btnClear = document.querySelector('#clear');
-    //listen for clear...
-    btnClear.addEventListener('click', clearGrid);
+//...initiate draw...
+function initDraw() {
+    //listen for draw effects...
+    listenForEffects();
+    //toggle draw...
+    checkDrawOn();
 }
 
 // ------------- //
 // GENERATE GRID //
 // ------------- //
 
-const gridContainer = document.querySelector('div.grid-container');
-let gridItems;
-//...draw grid elements & create array of grid items
-function drawGrid(gridWH) {
-    let gridSize = (gridWH ** 2)
-    for (let i=1; i<=gridSize; i++) {
-        //create grid item
+//...draw grid elements & capture gridItem nodeList
+function drawGrid(size) {
+    let gridSize = (size ** 2);
+    for (let i = 1; i <= gridSize; i++) {
+        //create gridItem
         const gridItem = document.createElement('div');
         gridItem.classList.add('grid-item');
-        //append grid item to grid container
+        //append gridItem to gridContainer
         gridContainer.appendChild(gridItem);
     }
     gridItems = gridContainer.querySelectorAll('div.grid-item');
@@ -108,47 +133,43 @@ function drawGrid(gridWH) {
 }
 
 //...layout grid structure
-const styleGrid = (gridWH) => {
-    gridContainer.style.gridTemplateColumns = `repeat(${gridWH}, 10px)`;
-    gridContainer.style.gridTemplateRows = `repeat(${gridWH}, 10px)`;
+const layoutGrid = (size) => {
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 10px)`;
+    gridContainer.style.gridTemplateRows = `repeat(${size}, 10px)`;
 }
 
-//...generate grid
-function genGrid(userInput) {
+//...generate grid...
+const gridContainer = document.querySelector('div.grid-container');
+function genGrid(size) {
     //layout grid...
-    styleGrid(userInput);
+    layoutGrid(size);
     //draw grid...
-    drawGrid(userInput);
-    //add effects...
-    listenForEffects();
+    drawGrid(size);
 }
 
-// ------------ //
-// INIT SCRIPTS //
-// ------------ //
-
-//TODO uncomment after debug
-// let size;
-// //...get width/height --> size
-// function getWH() {
-//     const maxWH = 70;
-//     const minWH = 10;
-//     while (!(size <= maxWH && size >= minWH)) {
-//         size = prompt('? x ?');
-//     }
-//     return size;
-// }
-
-function startSketch() {
-    //get width/height...
-    //TODO uncomment after debug
-    // getWH();
-    //generate grid...
-    //TODO 40 --> size after debug
-    genGrid(40);
+//...get width/height
+let size;
+function getWH() {
+    const maxWH = 70;
+    const minWH = 10;
+    while (!(size <= maxWH && size >= minWH)) {
+        size = prompt('? x ?');
+    }
+    return size;
 }
 
 // --------------- //
 // START SKETCHING //
 // --------------- //
+
+//start sketching...
+function startSketch() {
+    //get width/height...
+    getWH();
+    //generate grid...
+    genGrid(size);
+    //initiate draw...
+    initDraw();
+}
+
 let start = startSketch();
